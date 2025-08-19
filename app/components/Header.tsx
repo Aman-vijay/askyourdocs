16f51afcbfcd5182f86be83d7c73f4ce11ceae60
Aman-vijay <shivamvj04@gmail.com>
2025-08-19 22:18:03 +0530

    chore: update dependencies and add langchain packages

---
+++ app/components/Header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <div className="flex items-center justify-between bg-gradient-to-r from-blue-700 to-blue-500 p-6 shadow-md">
+            <div className="flex items-center">
+                <h1 className="text-2xl font-bold text-white tracking-wide">Ask Your Docs</h1>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Header;
